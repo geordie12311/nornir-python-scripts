6d39a2fcf1180189d1bb664425f50156366fcf11
Geordie12311 <80632286+geordie12311@users.noreply.github.com>
2022-01-29 07:43:23 +0000

    Update normiko-sendcmd.py
    #python script using nornir_netmiko to send show commands and print results

--- normiko-sendcmd.py
+++ normiko-sendcmd.py
@@ -1,3 +1,4 @@
+#python script using nornir_netmiko to send show commands and print results
 from nornir import InitNornir
 from nornir_netmiko.tasks import netmiko_send_command
 from nornir_utils.plugins.functions import print_result
