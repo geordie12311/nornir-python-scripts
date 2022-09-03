f315be3e8534fd7a04096f089a24a9325c82f150
geordie12311 <georgeharwood@live.co.uk>
2022-09-03 16:43:10 +0100

    updated

--- norscrap-showcomd.py
+++ norscrap-showcomd.py
@@ -1,9 +1,12 @@
 #simple python script to run a show command on hosts
+import getpass
 from nornir import InitNornir
 from nornir_scrapli.tasks import send_command
 from nornir_utils.plugins.functions import print_result
 
 nr = InitNornir(config_file="config.yaml")
+password = getpass.getpass()
+nr.inventory.defaults.password = password
 
 def show_command_test(task):
     task.run(task=send_command, command="show ip interface brief")
