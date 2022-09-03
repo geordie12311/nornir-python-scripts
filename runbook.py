efc8963c9169666f61338555102207a9546057e6
geordie12311 <georgeharwood@live.co.uk>
2022-09-03 16:36:26 +0100

    updates

--- runbook.py
+++ runbook.py
@@ -1,8 +1,13 @@
+# Test python runbook to pull ntp_server details from host file and apply to configuration
+import getpass
 from nornir import InitNornir
 from nornir_scrapli.tasks import send_config
 from nornir_utils.plugins.functions import print_result
 
 nr = InitNornir(config_file="config.yaml")
+password = getpass.getpass()
+nr.inventory.defaults.password = password
+#above section is going to prompt the user to put in their password
 
 def random_config(task):
     task.run(task=send_config, config=f"ntp server {task.host['ntp_server']}")
