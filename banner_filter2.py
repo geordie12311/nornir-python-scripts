c94157d33f0a41da9916214ff6fe58ddec40967a
geordie12311 <georgeharwood@live.co.uk>
2022-09-11 12:02:21 +0100

    updates

--- banner_filter2.py
+++ banner_filter2.py
@@ -4,7 +4,7 @@ pip3 install nornir
 pip3 install nornir_scrapli
 pip3 install nornir_utils
 """
-# Python script to filter on specific host attributes and apply site specific banner
+# Python script to filter on specific host attributes and apply site specifci banners
 import getpass
 from nornir import InitNornir
 from nornir_scrapli.tasks import send_configs_from_file
@@ -21,10 +21,11 @@ nr.inventory.defaults.password = password
 #The above lines will prompt the user to enter their username and password and use that input to connect to the devices
 
 def banner_push(task):
-    task.run(task=send_configs_from_file, name="Configuring the MOTD Banner", file="legend_banner.txt")
-# creating the function to use send_configs_from_file to send the txt from the legend_banner.txt file
+    task.run(task=send_configs_from_file, name="Configuring the MOTD Banner", file="tennis_legend_banner.txt")
+# creating the function to use send_configs_from_file to send the txt from the tennis_legend_banner.txt file
 
-legends = nr.filter(F(legend=True))
-# using F filter to filter hosts who have legend in their data profile
-results = legends.run(task=banner_push)
+legends_on_the_court = nr.filter(F(legend=True) & F(location="court"))
+# applying a filter to check for legend and also court to filter out hosts
+results = legends_on_the_court.run(task=banner_push)
+# running the results of the legends_on_the_court filter for the results
 print_result(results)
