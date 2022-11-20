7b27341a5274a8376fb4bee51af0aeac61671a21
geordie12311 <georgeharwood@live.co.uk>
2022-11-20 16:30:09 +0000

    new scripts

--- sendconfiglines_multihost_prompt.py
+++ sendconfiglines_multihost_prompt.py
@@ -1,13 +1,14 @@
 """
 Python script using nornir_scrapli, nornir_salt (FFun) as a filter
-to send a set of show commands to a list of hosts. Script will prompt 
-the user to provide credentials and provide hostnames + list of show commands
+to send a set of configuration commands to a set of hosts. Script will prompt 
+the user to provide credentials and the hostnames they want to configure
+and also the list of configuration commands
 """
 import getpass
 from nornir import InitNornir
-from nornir_utils.plugins.functions import print_result
-from nornir_scrapli.tasks import send_commands
+from nornir_scrapli.tasks import send_configs
 from nornir_salt.plugins.functions import FFun
+from nornir_utils.plugins.functions import print_result
 
 """
 Initiating Nornir
@@ -25,17 +26,17 @@ nr.inventory.defaults.password = password
 
 """
 Prompting user for the hostnames 
-and list of show commands
+and list of configuration commands
 """
-target = input("Choose hosts, comma separated list: ")
-cmds = input("Commands to send, comma separated list:  ")
+target = input("list the hosts you want to configure, (comma separated list): ")
+cmds = input("Configuration commands to send, (comma separated list):  ")
 
 """
 filering the hosts and sending the commands
 split by line using the , as the delimiter
 """
 filtered_hosts = FFun(nr, FL=target)
-output = filtered_hosts.run(send_commands, commands=cmds.split(","))
+output = filtered_hosts.run(send_configs, configs=cmds.split(","))
 
 """
 printing out the results 
