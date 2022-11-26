580e799c71129230d0a72160be0e434823bfdf2c
geordie12311 <georgeharwood@live.co.uk>
2022-11-26 11:12:17 +0000

    updated comments

--- sendconfiglines_multihost_prompt.py
+++ sendconfiglines_multihost_prompt.py
@@ -3,41 +3,32 @@ to send a set of configuration commands to a set of hosts. Script will prompt
 the user to provide credentials and the hostnames they want to configure
 and also the list of configuration commands """
 
-#importing the libraries
 import getpass
 from nornir import InitNornir
-from nornir_scrapli.tasks import send_configs
-from nornir_salt.plugins.functions import FFun
-from nornir_utils.plugins.functions import print_result
+from nornir_scrapli.tasks import send_configs # importing scrapli to send configuration commands
+from nornir_salt.plugins.functions import FFun #importing FFun for filtering
+from nornir_utils.plugins.functions import print_result #importing print_result to print the results to screen
 
-#Initiating Nornir
-nr = InitNornir(config_file="config.yaml")
+nr = InitNornir(config_file="config.yaml") #Initiating Nornir using config.yaml as configuration file
 
-#Prompting user for username and password. Using getpass for password
-# which will be used later in the script
-userNamePrompt = "Enter your username: "
-passPrompt = "Enter your password: "
 
-#Prompting user for the hostnames and list of configuration commands,
-# #which will be used later in the script
-targetPrompt = "list the hosts you want to configure, (comma separated list): "
-cmdPrompt = "Configuration commands to send, (comma separated list):  "
+userNamePrompt = "Enter your username: " #creating object for username prompt
+passPrompt = "Enter your password: " #creating object for password prompt
 
-#logging into the device(s) using user inputted name/password credentials
-userName = input(userNamePrompt)
-password = getpass.getpass(passPrompt)
-nr.inventory.defaults.username = userName
-nr.inventory.defaults.password = password
 
-#using the user inputted target device(s) and commands
-target = input(targetPrompt)
-cmds = input(cmdPrompt)
+targetPrompt = "list the hosts you want to configure, (comma separated list): "#promting user to enter host(s) details
+cmdPrompt = "Configuration commands to send, (comma separated list):  "#promting user to enter command(s) to send to host(s)
 
-#filering for the hosts and sending the commands split by line 
-#using the comma as the delimiter
-filtered_hosts = FFun(nr, FL=target)
-output = filtered_hosts.run(send_configs, configs=cmds.split(","))
+userName = input(userNamePrompt) #using user inputted username to pass username to host(s)
+password = getpass.getpass(passPrompt) #using getpass to pass the password to host(s)
+nr.inventory.defaults.username = userName #using userName input for username to login
+nr.inventory.defaults.password = password #using password input for password to authenticate
 
-#printing out the results
+target = input(targetPrompt) #using user inputted hosts details for targets
+cmds = input(cmdPrompt) # using user inputted commands to send to hosts
+
+filtered_hosts = FFun(nr, FL=target) # using FFun filter to filter the hosts
+output = filtered_hosts.run(send_configs, configs=cmds.split(",")) #splitting the commands using the comma as the delimiter
+
 results = (output)
-print_result(results)
+print_result(results) #printing out the results
