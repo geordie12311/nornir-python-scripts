cb064aac8aebdc84ec2afa2b358d9003656f36f1
geordie12311 <georgeharwood@live.co.uk>
2023-01-15 16:04:48 +0000

    new script

--- nornir_ip_route_check.py
+++ nornir_ip_route_check.py
@@ -1,16 +1,12 @@
-
+#!/usr/bin/env python3
 import getpass
 from ipaddress import ip_network, ip_address
 from nornir import InitNornir
 from nornir_scrapli.tasks import send_command
 from nornir_utils.plugins.functions import print_result #importing print_result to print the results to screen
+from rich import print as rprint
 
-nr = InitNornir(config_file="config3.yaml") #Initiating Nornir using config.yaml as configuration file
-
-
-### User credential section:
-userNamePrompt = "Enter your username: " #creating object for username prompt
-passPrompt = "Enter your password: " #creating object for password prompt
+nr = InitNornir(config_file="config4.yaml") #Initiating Nornir using config.yaml as configuration file
 
 ### User credential section:
 userNamePrompt = "Enter your username: " #creating object for username prompt
@@ -23,6 +19,7 @@ nr.inventory.defaults.password = password #using password input for password to 
 
 target = input("Enter the target IP address: ")
 ipaddr = ip_address(target)
+my_list = []
 
 def get_routes(task):
     response = task.run(task=send_command, command="show ip route")
@@ -31,14 +28,25 @@ def get_routes(task):
     for prefix in prefixes:
         net = ip_network(prefix)
         if ipaddr in net:
-            source_proto = prefixes[prefix]["source_protocol"]
-            if source_proto == "connected":
+            source_prot = prefixes[prefix]["source_protocol"]
+            if source_prot == "connected":
                 try:
                     outgoing_intf = prefixes[prefix]["next_hop"]["outgoing_interface"]
                     for intf in outgoing_intf:
                         exit_intf = intf
-                        print(f" {task.host} is connected to {target} via interface {exit_intf}")
+                        my_list.append(f"{task.host} is connected to {target} via interface {exit_intf}")
                 except KeyError:
                     pass
-        
+            else:
+                try:
+                    next_hop_list = prefixes[prefix]["next_hop_list"]
+                    for key in next_hop_list:
+                        next_hop = next_hop_list[key]["next_hop"]
+                        my_list.append(f"{task.host} can reach {target} via next hop {next_hop} ({source_prot})")
+                except KeyError:
+                    pass
+
 nr.run(task=get_routes)
+sorted_list = sorted(my_list)
+rprint(sorted_list)
+                        
