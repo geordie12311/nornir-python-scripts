615fccc03c75a81264e0acb87b76f63d0c0ed247
geordie12311 <georgeharwood@live.co.uk>
2023-01-15 16:01:20 +0000

    updated

--- nornir-multi-showcmd.py
+++ nornir-multi-showcmd.py
@@ -1,11 +1,12 @@
 #python script using nornir_scrapli to send multiple show commands
 #user will be prompted to add show commands on screen
+import getpass
 from nornir import InitNornir
 from nornir_scrapli.tasks import send_command
 from nornir_utils.plugins.functions import print_result
 #importing send_commands from nornir_scrapli to send multiple commands
 
-nr = InitNornir(config_file="config.yaml")
+nr = InitNornir(config_file="config4.yaml")
 #The above line is telling nornir where the config file is located
 user = input("Enter your username: ")
 password = getpass.getpass(prompt="Enter your password: ")
